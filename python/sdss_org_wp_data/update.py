7000f510b364b39fc7c49f6c8f5d79db390bb561
Joel Brownstein <joelbrownstein@astro.utah.edu>
2023-08-08 09:45:12 -0600

    update

---
+++ python/sdss_org_wp_data/update.py
@@ -0,0 +1,26 @@
+
+class Update:
+
+    def get_checksum(self):
+        return None
+        
+    def set_json(self):
+        self.json = None
+        
+    def set_data_from_json(self)
+        self.data = None
+        
+    def remove_duplicates(self):
+        pass
+        
+    def sort_data(self):
+        pass
+        
+    def set_checksum(self):
+        self.checksum = None
+        
+    def export_data_to_json(self):
+        pass
+        
+    def github_commit(self):
+        pass
