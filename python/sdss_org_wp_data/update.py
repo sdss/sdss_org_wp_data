db6fd012fbe4f8d304dcc1c641eab124c9742c17
Joel Brownstein <joelbrownstein@astro.utah.edu>
2023-08-08 10:07:20 -0600

    update

--- python/sdss_org_wp_data/update.py
+++ python/sdss_org_wp_data/update.py
@@ -1,10 +1,27 @@
+from os import environ
+from os.path import exists, join
 
 class Update:
 
+    def __init__(self, listname = None, listtype = None, verbose = None):
+        self.list = {'name': listname, 'type': listtype} if listname and listtype else None
+        self.verbose = verbose
+        self.set_wiki_data_dir()
+        
+    def set_wiki_data_dir(self):
+        try: self.wiki_data_dir = environ['WIKI_DATA_DIR']
+        except: self.wiki_data_dir = None
+        
     def get_checksum(self):
         return None
         
     def set_json(self):
+        if self.list and self.wiki_data_dir:
+            self.json = join(self.wiki_data_dir, 'members', "%(type)s", "%(name)s.json") % self.list
+            if not exists(self.json):
+                print("UPDATE> Nonexistent %r" % self.json)
+                self.json = None
+            elif self.verbose:  print("UPDATE> Found %r" % self.json)
         self.json = None
         
     def set_data_from_json(self):
